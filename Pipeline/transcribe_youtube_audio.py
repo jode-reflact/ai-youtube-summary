30a55f4eed542efc7e77d6f5da40a3bd912a151b
Yui-Senpai <git@shuvi.de>
2023-05-20 12:23:01 +0200

    moved transcirber to class

--- Pipeline/transcribe_youtube_audio.py
+++ Pipeline/transcribe_youtube_audio.py
@@ -3,21 +3,41 @@ import os
 
 import whisper
 
-input_folder = "Pipeline/temp/raw"
-input_audio = "Worst things to accidentally send your therapist.mp3"
-output_folder = "Pipeline/temp/transcribed"
+# input_folder = "Pipeline/temp/raw"
+# input_audio = "Worst things to accidentally send your therapist.mp3"
+# output_folder = "Pipeline/temp/transcribed"
 
 
-def transcribe_video():
-    model = whisper.load_model("medium")
-    result = model.transcribe(os.path.join(input_folder, input_audio))
-    r = json.dumps(result)
-    with open(
-        os.path.join(output_folder, input_audio + ".json"),
-        "w",
-    ) as f:
-        json.dump(result, f)
+class VideoTranscriber:
+    def __init__(self, input_folder, output_folder) -> None:
+        self.input_folder = input_folder
+        self.output_folder = output_folder
 
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
 
-if __name__ == "__main__":
-    transcribe_video()
+    def get_language(self, input_audio):
+        model = whisper.load_model("base")
+        # load audio and pad/trim it to fit 30 seconds
+        audio = whisper.load_audio(input_audio)
+        audio = whisper.pad_or_trim(audio)
+
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        # detect the spoken language
+        _, probs = model.detect_language(mel)
+        print(f"Detected language: {max(probs, key=probs.get)}")
+
+    def transcribe_video(self, input_audio):
+        model = whisper.load_model("medium")
+        result = model.transcribe(os.path.join(
+            self.input_folder, input_audio)+".mp3")
+        print(self.get_language(input_audio + ".mp3"))
+        r = json.dumps(result)
+        # print(result)
+        with open(
+            os.path.join(self.output_folder, input_audio + ".json"),
+            "w",
+        ) as f:
+            json.dump(result, f)
