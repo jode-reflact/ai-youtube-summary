38bf6f9898b91a6f176941239b59de8080b197c9
Yui-Senpai <git@shuvi.de>
2023-05-22 16:42:11 +0200

    bunch of random shit

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -1,9 +1,10 @@
-from download_youtube_audio import VideoDownloader
-from transcribe_youtube_audio import VideoTranscriber
+from src.download_youtube_audio import VideoDownloader
+from src.transcribe_youtube_audio import VideoTranscriber
+from src.summarize_video import videoSummary
 import json
-
 import os
-FULL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
 video_url = "https://www.youtube.com/watch?v=okSidIyw6GM"
 
 if __name__ == "__main__":
@@ -17,10 +18,22 @@ if __name__ == "__main__":
             config["downloader"]["video_output_folder"],
             config["downloader"]["transcribtion_output_folder"])
 
+        video_summarizer = videoSummary(
+            config["openai"]["organization"],
+            config["openai"]["api_key"],
+            config["openai"]["model"])
+
         # title = video_downloader.download_youtube_video(video_url)
         # print(title)
         # languages = video_transcriber.get_language(title + ".mp3")
         # print(languages)
         title = "Worst things to accidentally send your therapist"
-        text = video_transcriber.transcribe_video(title)
-        print(text)
+        base = config["downloader"]["video_output_folder"]
+        path = os.path.join(base, title)
+        path = path + ".mp3"
+        path = path.replace("\\", "/")
+        shark = video_transcriber.transcribe_video(title)
+        # print(shark)
+        print(video_summarizer.chat_completion(shark))
+        # text = video_transcriber.transcribe_video(title)
+        # print(text)
