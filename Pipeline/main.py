9d2a41abe49a2a45d8f68ef7b3532e52f053c6f8
Yui-Senpai <git@shuvi.de>
2023-05-25 11:22:34 +0200

    added pipeline fuction

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -1,13 +1,15 @@
-from src.download_youtube_audio import VideoDownloader
-from src.transcribe_youtube_audio import VideoTranscriber
-from src.summarize_video import videoSummary
 import json
-import os
+
+from src.download_youtube_audio import VideoDownloader
+from src.summarize_video import videoSummary
+from src.transcribe_youtube_audio import VideoTranscriber
+
+# video_url = "https://www.youtube.com/watch?v=okSidIyw6GM"  # short en test
+video_url = "https://www.youtube.com/watch?v=0vuzqunync8"  # longer en test
+# video_url = "https://www.youtube.com/watch?v=cPzSJiGR6aA"  # jp test
 
 
-# video_url = "https://www.youtube.com/watch?v=okSidIyw6GM"
-video_url = "https://www.youtube.com/watch?v=0vuzqunync8"
-if __name__ == "__main__":
+def pipeline(video_url):
     with open("Pipeline\config.json") as config_file:
         config = json.load(config_file)
 
@@ -17,8 +19,9 @@ if __name__ == "__main__":
         video_transcriber = VideoTranscriber(
             config["downloader"]["video_output_folder"],
             config["downloader"]["transcribtion_output_folder"],
-            config["downloader"]["english_model"],
-            config["downloader"]["alternate_model"])
+            config["whisper"]["english_model"],
+            config["whisper"]["alternate_model"],
+            config["whisper"]["base_model"])
 
         video_summarizer = videoSummary(
             config["openai"]["organization"],
@@ -26,16 +29,23 @@ if __name__ == "__main__":
             config["openai"]["model"])
 
         title = video_downloader.download_youtube_video(video_url)
-        print(title)
-        # languages = video_transcriber.get_language(title + ".mp3")
-        # print(languages)
-        # title = "Worst things to accidentally send your therapist"
-        base = config["downloader"]["video_output_folder"]
-        path = os.path.join(base, title)
-        path = path + ".mp3"
-        path = path.replace("\\", "/")
-        shark = video_transcriber.transcribe_video(title)
-        # print(shark)
-        # print(video_summarizer.chat_completion(shark))
-        # text = video_transcriber.transcribe_video(title)
-        # print(text)
+        text = video_transcriber.transcribe_video(title)
+        #   print(text + "\n")
+        summary = video_summarizer.chat_completion(text)
+        print(summary)
+        return summary
+
+
+if __name__ == "__main__":
+    pipeline(video_url)
+    # print(title)
+    # languages = video_transcriber.get_language(title + ".mp3")
+    # print(languages)
+    # title = "Worst things to accidentally send your therapist"
+    # path = video_transcriber._build_file_path(title)
+    # shark = video_transcriber.transcribe_video(title)
+
+    # shark = video_transcriber.get_language(title)
+    # shark = video_transcriber._load_model(title)
+    # print(video_transcriber._build_file_path(title))
+    # print(video_summarizer.chat_completion(shark))
