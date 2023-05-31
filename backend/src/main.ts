7f6a06bdf7168040437a48b1c598f725c775d84a
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2023-05-31 12:24:22 +0200

    log port number when starting backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './config/environment-variables';
@@ -7,8 +8,10 @@ import { EnvironmentVariables } from './config/environment-variables';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService<EnvironmentVariables>>(ConfigService);
+  const logger = new Logger(bootstrap.name);
 
   const BACKEND_PORT = config.get('BACKEND_PORT', { infer: true });
   await app.listen(BACKEND_PORT);
+  logger.log(`🚀 Server is running on port ${BACKEND_PORT}`);
 }
 bootstrap();
