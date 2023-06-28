226b35a307689c726ff07e18f6b45780919c50b5
Yui-Senpai <git@shuvi.de>
2023-06-28 16:05:08 +0200

    force convert mp3 using ffmpeg

--- Pipeline/src/transcribe_youtube_audio.py
+++ Pipeline/src/transcribe_youtube_audio.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import openai
 
 import whisper
@@ -15,6 +16,7 @@ class VideoTranscriber:
 
         self.use_whisper_api = use_whisper_api
         self.api_key = api_key
+
         if not os.path.exists(self.output_folder):
             os.makedirs(self.output_folder)
 
@@ -52,8 +54,29 @@ class VideoTranscriber:
         except Exception as e:
             print(e)
 
+
+    def _convert_to_mp3(self, input_file, output_file):
+        try:
+            # Execute the ffmpeg command
+            subprocess.run(['ffmpeg', '-y', '-i', input_file, '-codec:a', 'libmp3lame', output_file])
+            # print(f"Conversion successful. MP3 file saved as {output_file}")
+        except FileNotFoundError as e:
+            # print("ffmpeg not found. Please ensure that ffmpeg is installed and added to the system's PATH.")
+            print(e)
+
+    def _delete_file(self, file_path):
+        try:
+            # Delete the file
+            os.remove(file_path)
+            print(f"File deleted: {file_path}" + "\n")
+        except OSError as e:
+            print(f"Error occurred while deleting the file: {e}")
+
+    def _split_mp3_into_chunks_pydub(self, input_audio, output_folder):
+        pass
+
     def _split_mp3_into_chunks(self, input_audio, output_folder):
-        chunk_size = 10 * 1024 * 1024  # 25MB in bytes
+        chunk_size = 25 * 1024 * 1024  # 25MB in bytes
         file_name = os.path.basename(input_audio)
         
         with open(input_audio, 'rb') as mp3_file:
@@ -69,6 +92,11 @@ class VideoTranscriber:
                 with open(output_file_path, 'wb') as output_file:
                     output_file.write(chunk_data)
                 
+                # if index > 1:
+                # output_dir = os.path.dirname(output_file_path)
+                # os.makedirs(output_dir, exist_ok=True)
+                self._convert_to_mp3(output_file_path, output_file_path.replace(".mp3", "_converted.mp3"))
+                self._delete_file(output_file_path)
                 index += 1
 
     def _transcribe_video_local(self, input_audio):
@@ -82,15 +110,18 @@ class VideoTranscriber:
 
     def _transcribe_video_with_whisper_api(self, input_audio):
         result = ""
-        # self._split_mp3_into_chunks(self._build_file_path(input_audio), self.output_folder)
+        self._split_mp3_into_chunks(self._build_file_path(input_audio), self.output_folder)
         for chunk in os.listdir(self.output_folder):
             try:                
                 chunk_path = os.path.join(self.output_folder, chunk)
                 chunk_path = chunk_path.replace("\\", "/")
-                print(chunk_path)
+                print("\n" + chunk_path)
                 with open(chunk_path, "rb") as audio_file:
                     script = openai.Audio.transcribe("whisper-1", audio_file, api_key=self.api_key)
-                    result += script["text"]           
+                    
+                    # print(script["text"] + "\n")
+                    result += script["text"]        
+                self._delete_file(chunk_path)   
             except Exception as e:
                 print(e)
         return result
