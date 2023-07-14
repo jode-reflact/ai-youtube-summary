5c3bbe984862e13ed534a8c8810fc842f165a112
Jonas Deterding <jonas.deterding@reflact.com>
2023-07-14 14:03:45 +0200

    ✨ (dockerService): updated docker runner and added interval to delete exited containers

--- backend/src/docker/docker.service.ts
+++ backend/src/docker/docker.service.ts
@@ -10,26 +10,39 @@ export class DockerService {
         @InjectModel(Video.name)
         private readonly videoModel: Model<VideoDocument>,
     ) {
+        // intervall to delete exited containers
         /*
-        const process = child_process.spawn('docker', ["events", '--filter event=destroy', '--filter type=container', "--format '{{json .}}'"], { shell: true });
-        process.stdout.on('data', (data) => {
-            const d = Buffer.from(data);
-            const dataString = d.toString();
-            console.log('output', dataString);
-
-        })
+         setInterval(() => {
+            child_process.spawn('docker', ["container", "prune", "--force"], { shell: true });
+        }, 1000 * 60 * 1)
         */
     }
+
     // ytVideoUrl example: https://www.youtube.com/watch?v=L3wKzyIN1yk
     async summarizeYTVideo(ytVideoUrl: string) {
         return new Promise<string>((resolve, reject) => {
             const process = child_process.spawn('docker', ["run", '--env yt_url="' + ytVideoUrl + '"', 'ai-yt-summary'], { shell: true });
+            process.stderr.on('data', (data) => {
+                const d = Buffer.from(data);
+                const dataString = d.toString();
+                console.log('error:', dataString);
+                // docker error occured
+                if (dataString.includes('docker')) {
+
+                    process.stderr.removeAllListeners('data');
+                    process.stdout.removeAllListeners('data');
+                    reject(dataString)
+                }
+            });
             process.stdout.on('data', (data) => {
                 const d = Buffer.from(data);
                 const dataString = d.toString();
                 console.log('output', dataString);
                 if (dataString.startsWith("Summary:")) {
                     const summary = dataString.replace("Summary:", "")
+
+                    process.stderr.removeAllListeners('data');
+                    process.stdout.removeAllListeners('data');
                     resolve(summary)
                 }
             })
